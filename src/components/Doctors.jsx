fe33229adac56c2930202e37a56fdacafc641367
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-27 20:45:27 +0300

    home

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,9 +1,21 @@
+import { Col, Row } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
 
-const Doctors = () => {
+const Doctors = ({doctors}) => {
   return (
     <Container className="p-2">
-      <h1>Doctors</h1>
+      <h3 className="display-6" style={{ color: "rgb(166, 18, 189)" }}> Our Doctors</h3>
+
+      <Row>
+        {doctors.map((dr)=>(
+           <Col>
+       <img src={dr.img}/>
+       <h4>{dr.name}</h4>
+       <h5>{dr.dep}</h5>
+        </Col>
+        ))}
+       
+      </Row>
     </Container>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import AppointmentList from "../components/AppointmentList"
 import Doctors from "../components/Doctors"
-
+import {doctorData} from "../helper/data"
+import { useState } from "react"
 const Home = () => {
+  const [doctors, setDoctors] = useState(doctorData)
   return (
     <main className="text-center mt-2 vh-100">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
-      <Doctors />
+      <Doctors doctors={doctors}/>
       <AppointmentList />
     </main>
   )
