d61aeefd0d4fcb1e826540953a471c95e2ca3462
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-27 20:26:55 +0300

    hey

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container"
 const AppointmentList = () => {
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-2">Appointment Listt</h3>
+      <h3 className="display-6 mb-2">Appointment List</h3>
     </Container>
   )
 }
